387d940335889b222b7babbef0b54c068be6b24b
jambis <imjaamees@gmail.com>
2020-02-21 10:40:14 -0600

    Removed print and other unnecessary code

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -9,21 +9,12 @@ def count_th(word):
     total = 0
 
     if len(word) < 2:
-        print(word)
         return total
 
     elif word[:2] == "th":
-        print("elif ran")
         total = 1 + count_th(word[2:])
 
     else:
-        print("else ran")
         total = count_th(word[1:])
 
     return total
-
-
-word = "athcabc"
-# print(word[:2])
-
-print(count_th(word))
